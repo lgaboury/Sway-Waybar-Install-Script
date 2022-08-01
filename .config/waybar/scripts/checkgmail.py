ef6822f9aacec1f7f5d806e9252e270bf884807d
Luc Gaboury <50297059+lgaboury@users.noreply.github.com>
2022-08-01 16:22:24 -0500

    Update checkgmail.py

--- .config/waybar/scripts/checkgmail.py
+++ .config/waybar/scripts/checkgmail.py
@@ -3,7 +3,7 @@
 import imaplib,time
 cnt = 0
 obj = imaplib.IMAP4_SSL('imap.gmail.com','993')
-obj.login('lucgaboury@gmail.com','nakaoqnvrhddjzsw')
+obj.login('google username','password')
 obj.select()
 typ, data = obj.search(None,'UnSeen')
 for num in data[0].split():
