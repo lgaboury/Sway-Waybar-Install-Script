bf9634a5b72e1fb98b5e7942f3483255a98173c5
Luc Gaboury <50297059+lgaboury@users.noreply.github.com>
2022-08-01 16:18:14 -0500

    Delete get_bing_image_name.sh

--- .config/sway/scripts/get_bing_image_name.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-baseurl="https://www.bing.com/"
-imageName=$(curl $baseurl"HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-CA" -s | jq '.images[].copyright' --raw-output)
-echo $imageName
