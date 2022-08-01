9ab2f1a00a15f00e95e5e222d75acacf6090c637
Luc Gaboury <50297059+lgaboury@users.noreply.github.com>
2022-08-01 16:40:25 -0500

    Update get_bing_image.sh

--- .config/sway/scripts/get_bing_image.sh
+++ .config/sway/scripts/get_bing_image.sh
@@ -2,41 +2,41 @@
 
 DIR="$HOME/.config/sway/wallpapers/"
 
-# exit on error
-set -e
-
-# set $SWAYSOCK if it's not set (for systemd or cron)
-if [ -z "$SWAYSOCK" ]; then
-  export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
-fi
-
 # Check if sway/wallpapers directory exists, if not create it.
 if [ ! -d "$DIR" ]; then
   mkdir -p $DIR
 fi
 
+# Wallpaper path
 wlpath="$HOME/.config/sway/wallpapers/wallpaper.jpg"
+# Lockscreen wallpaper path
 lswlpath="$HOME/.config/sway/wallpapers/lockscreen_wallpaper.jpg"
 output="*"
 baseurl="https://www.bing.com/"
 
-while [ -z $wluri ]; do
-  wluri=$(curl $baseurl"HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-CA" -s | jq '.images[].url' --raw-output)
-  # wluri=$(curl $baseurl"HPImageArchive.aspx?format=js&idx=0&n=1" -s | jq '.images[].url' --raw-output)
-  # sleep 1
+# Get URL for Bing Image Of The Day for Canada
+while [ -z $wlurl ]; do
+  wlurl=$(curl $baseurl"HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-CA" -s | jq '.images[].url' --raw-output)
 done
 
+# Get name for Bing Image of the Day
 imageName=$(curl $baseurl"HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-CA" -s | jq '.images[].copyright' --raw-output)
 
+# Save Bing image name
 echo $imageName > $HOME/.config/sway/wallpapers/bing_name.txt
 
-curl "$baseurl$wluri" -s > $wlpath
+# Download and save Bing Image of the Day
+curl "$baseurl$wlurl" -s > $wlpath
 
+# Ensure no instances of swaybg and nwg-wrapper are already running
 killall swaybg || true
 killall nwg-wrapper || true
 
-#feh --bg-fill $wlpath
+# Set bing image of the day as wallpaper
 swaymsg "output $output bg $wlpath fill"
+
+# Display image name on bottom right of the wallpaper
 nwg-wrapper -t $HOME/.config/sway/wallpapers/bing_name.txt -c $HOME/.config/sway/scripts/bing_name.css -p right -a end -mb 10 -mr 10 &
 
+# Blur existing wallpaper to user later as a lock screen
 convert $wlpath -filter Gaussian -blur 0x8 -level 10%,90%,0.5 $lswlpath
