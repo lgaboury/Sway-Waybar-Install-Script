de1832a66773ed22c483fb8f08a1104dfeedba98
lgaboury <lucgaboury@gmail.com>
2022-09-04 16:16:34 -0500

    Updated check_battery script so notification does not repeat

--- .config/waybar/scripts/check_battery.sh
+++ .config/waybar/scripts/check_battery.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
+bat=/sys/class/power_supply/BAT0
 CRIT=${1:-15}
 
-for capacity in $(find /sys/class/power_supply/*/ -type f -name capacity)
-do
-	stat=$(cat ${capacity/capacity/status})
-	perc=$(cat $capacity)
-	bat=$(echo $capacity | cut -d'/' -f5)
-	if [ $perc -le $CRIT ] && [ $stat == "Discharging" ]; then
-		if [ ! -f notified ]; then
-			notify-send --urgency=critical --icon=dialog-warning "Battery Low" "Current charge of $bat: $perc%"
-			touch notified
-		fi
-	else
-		if [ -f notified ]; then
-			rm notified
-		fi
-	fi
-done
+FILE=~/.config/waybar/scripts/notified
 
+stat=$(cat $bat/status)
+perc=$(cat $bat/capacity)
+
+if [[ $perc -le $CRIT ]] && [[ $stat == "Discharging" ]]; then
+  if [[ ! -f "$FILE" ]]; then
+    notify-send --urgency=critical --icon=dialog-warning "Battery Low" "Current charge: $perc%"
+    touch $FILE
+  fi
+elif [[ -f "$FILE" ]]; then
+  rm $FILE
+fi
